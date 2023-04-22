9631ce14ccfff71a5fbc458bf771ef5ac2ec2957
Lorenzo Sorce <lorenzosorce@Mac-Lollo.local>
2023-04-22 18:59:38 +0200

    feat: base class, start market pricerù

--- pricer/market_pricing.py
+++ pricer/market_pricing.py
@@ -7,7 +7,41 @@ class MarketPricer(OptionPricer):
         self.base_url = base_url
 
     def fetch_option_data(self, option_symbol):
-        # Implement API call to fetch option data
+        url = f"{self.base_url}public/get_book_summary_by_currency?currency={option_symbol}&kind=option"
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            raise Exception(f"Failed to fetch option data from Deribit API: {response.text}")
+
+        option_data = response.json()
+        return option_data
 
     def interpolate(self, target_strike, option_data):
-        # Implement interpolation logic
+    # Implement interpolation logic
+    # This might involve finding the two closest available strikes in the option_data
+    # and computing a weighted average based on their prices and strikes.
+
+    # Assuming the option_data is a list of dictionaries containing 'strike' and 'price' keys
+        lower_data = None
+        upper_data = None
+
+        for data in option_data:
+            strike = data['strike']
+
+            if strike <= target_strike:
+                if lower_data is None or strike > lower_data['strike']:
+                    lower_data = data
+            elif strike > target_strike:
+                if upper_data is None or strike < upper_data['strike']:
+                    upper_data = data
+
+        if lower_data is None or upper_data is None:
+            raise ValueError("Could not find suitable data points for interpolation.")
+
+        lower_weight = (upper_data['strike'] - target_strike) / (upper_data['strike'] - lower_data['strike'])
+        upper_weight = 1 - lower_weight
+
+        interpolated_price = lower_weight * lower_data['price'] + upper_weight * upper_data['price']
+
+        return interpolated_price
+
--- pricer/option_pricer.py
+++ pricer/option_pricer.py
@@ -1,4 +1,31 @@
 class OptionPricer:
     def __init__(self):
-        pass
+        self.underlying_price = None
+        self.strike = None
+        self.time_to_expiry = None
+        self.option_type = None
+        self.interest_rate = None
 
+    def initialize_parameters(self, underlying_price, strike, time_to_expiry, option_type, interest_rate=None):
+        self.underlying_price = underlying_price
+        self.strike = strike
+        self.time_to_expiry = time_to_expiry
+        self.option_type = option_type
+        self.interest_rate = interest_rate if interest_rate is not None else 0
+
+        self.validate_inputs()
+
+    def compute_price(self):
+        raise NotImplementedError("The 'compute_price' method should be implemented in each child class.")
+
+    def validate_inputs(self):
+        if self.underlying_price <= 0:
+            raise ValueError("Underlying price must be positive.")
+        if self.strike <= 0:
+            raise ValueError("Strike price must be positive.")
+        if self.time_to_expiry <= 0:
+            raise ValueError("Time to expiry must be positive.")
+        if self.option_type not in ['call', 'put']:
+            raise ValueError("Option type must be either 'call' or 'put'.")
+        if self.interest_rate < 0:
+            raise ValueError("Interest rate must be non-negative.")
