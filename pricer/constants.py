bfbedd3b562527b1365778d5bd1823f07b970f3a
Lorenzo <lorenzo.sorce@hotmail.com>
2023-04-23 23:40:35 +0200

    Save progress before switching branches

---
+++ pricer/constants.py
@@ -0,0 +1,12 @@
+COIN_GECKO_IDS = {
+    'BTC': 'bitcoin',
+    'ETH': 'ethereum',
+    'LTC': 'litecoin',
+    'BCH': 'bitcoin-cash',
+    'BNB': 'binancecoin',
+    'EOS': 'eos',
+    'XLM': 'stellar',
+    'ADA': 'cardano',
+    'XMR': 'monero',
+    'TRX': 'tron'
+}
--- pricer/market_pricing.py
+++ pricer/market_pricing.py
@@ -1,16 +1,19 @@
-from .option_pricer import OptionPricer
+from option_pricer import OptionPricer
+from constants import COIN_GECKO_IDS
 import requests
 from datetime import datetime, timedelta
 
 class MarketPricer(OptionPricer):
-    instruments_cache = {}  
+    instruments_cache = {}
 
-    def __init__(self, input_string, base_url="https://www.deribit.com/api/v2/"):
+    def __init__(self, input_string, quantity, update_cache=True):
         super().__init__()
-        self.base_url = base_url
         self.input_string = input_string
-        self.parse_option_string(input_string)
-        self.update_instruments_cache(input_string)
+        self.base_url = "https://www.deribit.com/api/v2/"
+        self.parse_option_string(input_string, quantity)
+        if update_cache:
+            self.update_instruments_cache(input_string)
+        self.price = self.compute_price()
 
     def fetch_options_instruments(self):
         url = f"{self.base_url}public/get_book_summary_by_currency?currency={self.option_underlying}&kind=option"
@@ -20,6 +23,9 @@ class MarketPricer(OptionPricer):
             raise requests.exceptions.RequestException(f"Failed to fetch option data from Deribit API: {response.text}")
 
         book_data = response.json()
+        if "result" not in book_data:
+            raise ValueError("Unexpected response format from Deribit API")
+        
         instrument_names = [entry["instrument_name"] for entry in book_data["result"]]
         return instrument_names
 
@@ -29,11 +35,11 @@ class MarketPricer(OptionPricer):
 
         if option_underlying not in cls.instruments_cache:
             cls.instruments_cache[option_underlying] = {"timestamp": None, "instruments": []}
-
+            
         cache_entry = cls.instruments_cache[option_underlying]
-
+        
         if force_update or cache_entry["timestamp"] is None or datetime.now() - cache_entry["timestamp"] > timedelta(days=1):
-            instance = cls(input_string)  # Create an instance with the user's input string to fetch instruments
+            instance = cls(input_string, 1, update_cache=False)
             cache_entry["instruments"] = instance.fetch_options_instruments()
             cache_entry["timestamp"] = datetime.now()
 
@@ -49,25 +55,69 @@ class MarketPricer(OptionPricer):
                 raise requests.exceptions.RequestException(f"Failed to fetch option book data from Deribit API: {response.text}")
 
             order_book_data = response.json()
-            return order_book_data
+            return order_book_data['result']
         else:
             # If the option is not available, handle interpolation (to be implemented later)
-            raise NotImplementedError("Interpolation for unavailable options has not been implemented yet.")
-
+            raise NotImplementedError("Option not available, interpolation has not been implemented yet.")
     
-    def get_weighted_price(self, quantity):
-        # Check if the option is available in the instruments cache
-        if self.input_string not in self.instruments_cache["instruments"]:
-            raise ValueError("Requested option is not available.")
 
-        # Fetch the option book for the given option
-        option_book = self.fetch_option_book()
+    def fetch_spot_price(self, currency='usd'):
+        coin_id = COIN_GECKO_IDS.get(self.option_underlying)
 
-        # Compute the weighted price based on the quantity
-        # implement the logic for calculating the weighted price based on the option book data and the given quantity
-        # ...
+        if coin_id:
+            url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies={currency}"
+            response = requests.get(url)
 
-        return weighted_price
+            if response.status_code == 200:
+                data = response.json()
+                spot_price = data.get(coin_id, {}).get(currency)
+                if spot_price:
+                    return spot_price
+
+        # Fallback to Deribit API if CoinGecko fails or asset not found
+        url = f"{self.base_url}public/get_index?currency={self.option_underlying}_USDC"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            index_price = data.get("result", {}).get("last_price")
+            if index_price:
+                return index_price
+
+        # Raise error if both attempts fail
+        raise ValueError(f"Failed to fetch spot price for {self.option_underlying}")
+
+    def get_weighted_price(self, use_future_price=True):
+        order_book = self.fetch_option_book()
+        bids = order_book['bids']
+        asks = order_book['asks']
+        quantity = self.quantity
+
+        if use_future_price or 'underlying_price' not in order_book:
+            underlying_price = order_book['underlying_price']
+        else:
+            underlying_price = self.fetch_spot_price()
+        
+        def weighted_price(order_list, target_quantity):
+            total_price = 0
+            total_qty = 0
+
+            for price, size in order_list:
+                if total_qty + size <= target_quantity:
+                    total_price += price * size
+                    total_qty += size
+                else:
+                    remaining_qty = target_quantity - total_qty
+                    total_price += price * remaining_qty
+                    break
+
+            return total_price / target_quantity
+
+        bid_weighted_price = weighted_price(bids, quantity) * underlying_price
+        ask_weighted_price = weighted_price(asks, quantity) * underlying_price
+
+        return bid_weighted_price, ask_weighted_price
+
     
     def get_market_price(self, quantity):
         asset, date_str, strike, option_type = self.parse_option_string(self.input_string)
@@ -109,4 +159,11 @@ class MarketPricer(OptionPricer):
         interpolated_price = lower_weight * lower_data['price'] + upper_weight * upper_data['price']
 
         return interpolated_price
+    
+    def compute_price(self):
+        # Placeholder, will be implemented as last step
+        pass
 
+if __name__ == "__main__":
+    price = MarketPricer(input_string='BTC-26MAY23-23000-P', quantity=1)
+    price.get_weighted_price()
